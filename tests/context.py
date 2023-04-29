40c8d788d26315be2878563c6cd3ef1e4f86645b
Jon Tyler <k98kurz@gmail.com>
2023-04-29 04:39:55 -0400

    renamed directory to merkleasy

--- tests/context.py
+++ tests/context.py
@@ -2,6 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import merkle
-from merkle import classes
-from merkle import interfaces
+import merkleasy
+from merkleasy import classes
+from merkleasy import interfaces
