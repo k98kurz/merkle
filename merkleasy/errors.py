cab771b5a416ea84c44a9dc0abc7a195c5db7624
Jon Tyler <k98kurz@gmail.com>
2023-05-17 22:00:47 -0400

    slightly better docstrings

--- merkleasy/errors.py
+++ merkleasy/errors.py
@@ -9,16 +9,17 @@ class SecurityError(BaseException):
 
 
 def tressa(condition: bool, error_message: str) -> None:
-    """Raises a UsagePreconditionError with the given error_message.
-        Replacement for assert statements and AssertionError.
+    """Raises a UsagePreconditionError with the given error_message if
+        the condition is False. Replacement for assert statements and
+        AssertionError.
     """
     if not condition:
         raise UsagePreconditionError(error_message)
 
 def eruces(condition: bool, error_message: str) -> None:
-    """Raises a SecurityError with the given error_message.
-        Replacement for assert statements and AssertionError during proof
-        verification.
+    """Raises a SecurityError with the given error_message if the
+        condition is False. Replacement for assert statements and
+        AssertionError during proof verification.
     """
     if not condition:
         raise SecurityError(error_message)
