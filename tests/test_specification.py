be7b0754fb5bd34a044bb0fc08b1df93859e6f4d
Jon Tyler <k98kurz@gmail.com>
2023-05-17 21:38:32 -0400

    added specification test

---
+++ tests/test_specification.py
@@ -0,0 +1,6 @@
+from specifications.merkletree import MerkleTreeProtocol, check_module
+from context import merkleasy
+
+
+check_module(merkleasy, {merkleasy.Tree: MerkleTreeProtocol})
+print('specification test complete')
