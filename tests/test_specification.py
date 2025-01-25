e5ae486ad01774990abbeb8f5a449ccee5f8af88
Jonathan Voss <k98kurz@gmail.com>
2025-01-24 22:13:08 -0500

    disabling this specification test until I have time to revive that project

--- tests/test_specification.py
+++ tests/test_specification.py
@@ -2,4 +2,4 @@ from specifications.merkletree import MerkleTreeProtocol, check_module
 from context import merkleasy
 
 
-check_module(merkleasy, {merkleasy.Tree: MerkleTreeProtocol})
+# check_module(merkleasy, {merkleasy.Tree: MerkleTreeProtocol})
